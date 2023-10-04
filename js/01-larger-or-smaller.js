a0f9d4d12d357433b9c498eb09d54481caafe38c
Orion <Orion@Email.com>
2023-10-03 22:29:06 -0700

    init commit

---
+++ js/01-larger-or-smaller.js
@@ -0,0 +1,10 @@
+let num1 = prompt("Please Enter A Number.")
+let num2 = prompt("Please Enter Another Number")
+
+if (num1 < num2) {
+document.write(num2)
+} else if (num1 > num2) {
+document.write(num1)
+} else if (num1 === num2){
+document.write(num1)
+}
---
+++ js/02-word-translator.js
@@ -0,0 +1,11 @@
+let lang = prompt('Please Enter Your Language Code')
+switch (lang) {
+    case 'es':
+        document.write('Hello World in Spanish is ')
+        break
+    case "de"
+        document.write 
+}
+
+
+"es", "de", "en", and "fr"
